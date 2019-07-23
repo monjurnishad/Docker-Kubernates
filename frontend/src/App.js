2c435326912767646614db9405470133d5a262d6
Monjur Hasan <monjur.nishad@gmail.com>
2019-07-22 20:39:57 -0400

    added travis CI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,10 +8,10 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to Docker-React project!</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+           Deployed by Monjur Hasan on feature branch.
         </p>
       </div>
     );
